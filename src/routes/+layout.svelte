e316a30b87b217a47cb28fd46c1dbf7a1a84035e
Kuro <hello@kurojifusky.com>
2025-07-19 19:44:32 +0800

    update to use runes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-  import "../app.css"
-  import { GlobalSidebar, Navbar } from "$lib/components/Base"
+  import "../app.css";
+  import { GlobalSidebar, Navbar } from "$lib/components/Base";
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    children: Snippet;
+  }
+
+  const { children }: Props = $props();
 </script>
 
 <Navbar />
 <div class="flex h-[calc(theme(height.dvh)-theme(spacing.16))]">
   <GlobalSidebar />
   <main id="skip-to-content" class="flex-1 py-1.5 px-8">
-    <slot />
+    {@render children()}
   </main>
 </div>
